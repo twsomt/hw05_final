946567a3da9027e7bf2941745d3ab2659d8a0195
twsomt <volk0v-ak@yandex.ru>
2023-04-09 18:37:51 +0300

    Deactivate cache

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -2,9 +2,10 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.http import HttpResponseForbidden
 from django.shortcuts import get_object_or_404, redirect, render
-from django.views.decorators.cache import cache_page
+# from django.views.decorators.cache import cache_page
 
-from .constants import CACHE_TIMER, LEN_TITLE_POST_DETAIL
+from .constants import LEN_TITLE_POST_DETAIL
+# from .constants import CACHE_TIMER
 from .forms import CommentForm, PostForm
 from .models import Follow, Group, Post
 from .paginator import paginator
